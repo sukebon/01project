f2004b728115a50cace9e606a1903113a2af2021
sukebon <mukaibook@gmail.com>
2021-08-21 11:28:43 +0900

    fileter

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ const Layout: React.FC = (props) => {
   const { children } = props;
   return (
     <>
-      <div className="flex">
+      <div className="flex min-h-screen">
         <header className={`shadow-md`}>
           <Header />
         </header>
