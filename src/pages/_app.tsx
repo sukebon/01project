493fcd4c42d2e8aa361decde1fe09b3640262bd0
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-08-29 11:58:37 +0900

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,22 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
 import { TopicsProvider } from "context/TopicsContext";
 
+import * as gtag from "../../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <TopicsProvider>
       <Layout>
